c22f0fa4c539c333f84aae233f5c7ee96a02a81f
Diptanil Sen <150945082+Diptanil-Sen@users.noreply.github.com>
2026-05-25 11:10:54 +0530

    Update MainActivity.kt

--- button_click_listener_lambda/MainActivity.kt
+++ button_click_listener_lambda/MainActivity.kt
@@ -3,7 +3,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
