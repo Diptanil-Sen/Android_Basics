ebda4a45c581e7afd525397889f9c3c2cf281aa7
Diptanil Sen <150945082+Diptanil-Sen@users.noreply.github.com>
2026-07-08 20:31:25 +0530

    Update MainActivity.kt

--- RecycleView/MainActivity.kt
+++ RecycleView/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recycleview.Adaptor.ExampleAdaptor
+
 import com.example.recycleview.Model.ExampleItem
 
 class MainActivity : AppCompatActivity() {
